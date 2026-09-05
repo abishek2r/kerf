8acb79da2c7edd958a03aa164241a36fe2ad7d9f
abishek2r <abishek2r@gmail.com>
2026-09-05 17:35:09 +0530

    added store class for recording defect images and retreive it

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <iostream>
 #include <chrono>
+#include <vector>
+#include <span>
 template <typename T>
 double benchmark(T t,int iterations){
     auto totaltime = 0.0;
@@ -22,51 +24,38 @@ class Image{
     
     std::size_t width;
     std::size_t height;
-    std::uint8_t* pixels;
+    std::unique_ptr<std::uint8_t[]> pixels;
     public:
-    Image(std::size_t width_,std::size_t height_) : width(width_),height(height_),pixels(new std::uint8_t[width_*height_*3]){}
-    ~Image(){
-        delete[] pixels;
-    }
+    Image(std::size_t width_,std::size_t height_) : width(width_),height(height_),pixels(std::make_unique<std::uint8_t[]>(width_*height_*3)){}
+    ~Image() = default;
     
-    Image(const Image& other) : width(other.width),height(other.height),pixels(new std::uint8_t[other.width * other.height * 3]){
-        std::memcpy(pixels,other.pixels,other.width*other.height*3);
+    Image(const Image& other) : width(other.width),height(other.height),pixels(std::make_unique<std::uint8_t[]>(other.width * other.height * 3)){
+        std::memcpy(pixels.get(),other.pixels.get(),other.width*other.height*3);
     }
     
     Image& operator=(const Image& other){
         if (this != &other){
             
-            auto* new_pixels = new std::uint8_t[other.width * other.height * 3];
-            std::memcpy(new_pixels,other.pixels,other.width*other.height*3);
+            std::unique_ptr<std::uint8_t[]> new_pixels = std::make_unique<std::uint8_t[]>(other.width * other.height * 3);
+            std::memcpy(new_pixels.get(),other.pixels.get(),other.width*other.height*3);
             width = other.width;
             height = other.height;
-            delete[] pixels;
-            pixels = new_pixels;
+            pixels = std::move(new_pixels);
             
         }
         return *this;
-        
+         
     }
-    Image(Image&& other) noexcept : width(other.width),height(other.height),pixels(other.pixels){
-        other.width=0;
-        other.height = 0;
-        other.pixels=nullptr;
-    }
-    Image& operator=(Image&& other) noexcept
-    {
-        if (this != &other){
-            delete[] pixels;
-            pixels = other.pixels;
-            width = other.width;
-            height = other.height;
-            
-            other.pixels=nullptr;
-            other.height=0;
-            other.width = 0;
-        }
-        return *this;
-    }
+    Image(Image&& other) noexcept = default;
+    Image& operator=(Image&& other) noexcept = default;
     
+};
+class store{
+    std::vector<std::unique_ptr<Image>> records;
+    std::vector<std::unique_ptr<Image>>& getRecords() {
+        return records;
+    }
+
 };
 int main()
 {   Image a(10000,10000);
